5b3724d325ef436f709a97054921ab3f801ab5fc
Olivier Le Merdy <olemerdy@financeactive.com>
2012-10-29 09:32:09 +0100

    Last training day

--- src/main/java/com/financeactive/taptrain/pages/Contact.java
+++ src/main/java/com/financeactive/taptrain/pages/Contact.java
@@ -1,18 +1,18 @@
 package com.financeactive.taptrain.pages;
 
 import com.financeactive.taptrain.entities.User;
-import com.google.common.base.Predicate;
 import org.apache.tapestry5.EventConstants;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.hibernate.Session;
 
-import javax.annotation.Nullable;
 import javax.inject.Inject;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static com.financeactive.taptrain.entities.User.Properties.firstName;
+import static com.google.common.base.Predicates.contains;
 import static com.google.common.collect.FluentIterable.from;
 import static org.hibernate.criterion.Projections.distinct;
 import static org.hibernate.criterion.Projections.property;
@@ -31,16 +31,15 @@ public class Contact {
 
     @OnEvent(EventConstants.ACTIVATE)
     void initList() {
-        list = session.createCriteria(User.class).setProjection(distinct(property(firstName))).list();
+        list = session.createCriteria(User.class)
+                .setProjection(distinct(property(firstName)))
+                .list();
     }
 
     @OnEvent(EventConstants.PROVIDE_COMPLETIONS)
     List<String> autoComplete(final String start) {
-        return from(list).filter(new Predicate<String>() {
-            @Override
-            public boolean apply(@Nullable String s) {
-                return s != null && s.toLowerCase().startsWith(start.toLowerCase());
-            }
-        }).toImmutableList();
+        return from(list)
+                .filter(contains(Pattern.compile('^' + start, Pattern.CASE_INSENSITIVE)))
+                .toImmutableList();
     }
 }
